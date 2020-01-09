857cde606fa1e7b02cec163e2f83ba6a6f60fd88
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-01-09 16:06:28 +0100

    :white_check_mark: test: modif test

--- test/mlx.c
+++ test/mlx.c
@@ -6,7 +6,7 @@
 /*   By: jereligi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 09:42:41 by jereligi          #+#    #+#             */
-/*   Updated: 2020/01/09 14:17:45 by jereligi         ###   ########.fr       */
+/*   Updated: 2020/01/09 15:59:37 by jereligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,9 @@ int					ft_key_mouve(t_mlx *mlx)
 
 int					ft_get_key(t_mlx *mlx)
 {
-	write(1, "b", 1);	
+	write(1, "b", 1);
+	mlx->x++;
+	mlx->y++;
 	return (1);
 }
 
@@ -54,10 +56,18 @@ int					main(void)
 		return (-1);
 	if ((mlx.win = mlx_new_window(mlx.ptr, 640, 480, "Cub2D")) == NULL)
 		return (-1);
+	mlx_expose_hook(mlx.win, &ft_key_mouve, &mlx);
+	mlx_loop(mlx.ptr);
+	return (0);
+}
+
+/*	key_press (2, 0)
+	key_release (3 , 0)
+	mlx_hook
+	mlx.img = mlx_new_image(mlx.ptr, 640, 480);		
 	mlx.img = mlx_new_image(mlx.ptr, 640, 480);		
 	mlx.data_img = mlx_get_data_addr(mlx.img, &mlx.bpixel, &mlx.size_line, &mlx.endian);
-	mlx_expose_hook(mlx.win, &ft_key_mouve, &mlx);
-	mlx_key_hook(mlx.win, &ft_get_key, &mlx);
+
 	printf("bpixel %d\n", mlx.bpixel);
 	printf("size_line %d\n", mlx.size_line);
 	mlx.bpixel = mlx.bpixel / 8;
@@ -67,8 +77,6 @@ int					main(void)
 		i++;
 	}
 	mlx_put_image_to_window(mlx.ptr, mlx.win, mlx.img, 0, 0);
-	mlx_destroy_image(mlx.ptr, mlx.img);
+	mlx_destroy_image(mlx.ptr, mlx.img);*/
 //	mlx_clear_window(mlx.ptr, mlx.win);
-	mlx_loop(mlx.ptr);
-	return (0);
-}
+
