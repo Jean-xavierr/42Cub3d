ac7958a13cc672896303c6cf27b87d2dfea78a9a
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-01-14 13:00:03 +0100

    :arrow_up: test: new test with Minibilix

--- test/mlx.c
+++ test/mlx.c
@@ -6,7 +6,7 @@
 /*   By: jereligi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 09:42:41 by jereligi          #+#    #+#             */
-/*   Updated: 2020/01/09 15:59:37 by jereligi         ###   ########.fr       */
+/*   Updated: 2020/01/14 11:05:40 by jereligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,73 +26,152 @@ typedef	struct		s_mlx
 	int				bpixel;
 	int				size_line;
 	int				endian;
-	int				x;
-	int				y;
 }					t_mlx;
 
-dd
-typedef struct			s_keyinfo
+typedef struct			s_move
 {
+	int					esc;
 	int					foward;
 	int					retreat;
 	int					left;
 	int					right;
 	int					turn_left;
 	int					turn_right;
-}						t_keyinfo
+	int					sprint;
+}						t_move;
 
-int					ft_key_management(t_mlx)
+typedef struct			s_player
 {
+	int					x;
+	int					y;
+}						t_player;
 
+int					ft_key_management(t_mlx *mlx)
+{
+	return (0);
 }
 
-int					ft_keyrelease(int key, t_mlx *mlx)
+int					ft_keyrelease(int keycode, t_move *move)
 {
-
+	printf("\nkeycode |%d| : ", keycode);
+	if (keycode == 53)
+		move->esc = 0;
+	else if (keycode == 13)
+		move->foward = 0;
+	else if (keycode == 1)
+		move->retreat = 0;
+	else if (keycode == 0)
+		move->left = 0;
+	else if (keycode == 2)
+		move->right = 0;
+	else if (keycode == 123)
+		move->turn_left = 0;
+	else if (keycode == 124)
+		move->turn_right = 0;
+	else if (keycode == 257)
+		move->sprint = 1;
+	printf("0");
+	return (0);
 }
 
-int					ft_keypress(int key, t_mlx *mlx)
+int					ft_keypress(int keycode, t_move *move)
 {
-	if (key == 53)
+	printf("\nkeycode |%d| : ", keycode);
+	if (keycode == 53)
+	{
+		move->esc = 1;
 		printf("esc");
-	else if (key == 13)
+	}
+	else if (keycode == 13)
+	{
+		move->foward = 1;
 		printf("forward");
-	else if (key == 1)
+	}
+	else if (keycode == 1)
+	{
+		move->retreat = 1;
 		printf("retreat");
-	else if (key == 0)
+	}
+	else if (keycode == 0)
+	{
+		move->left = 1;
 		printf("left");
-	else if (key == 2)
+	}
+	else if (keycode == 2)
+	{
+		move->right = 1;
 		printf("right");
-	else if (key == 123)
+	}
+	else if (keycode == 123)
+	{
+		move->turn_left = 1;
 		printf("turn left");
-	else if (key == 124)
-		printf("turn  right");
+	}
+	else if (keycode == 124)
+	{
+		move->turn_right = 1;
+		printf("turn right");
+	}
+	else if (keycode == 257)
+	{
+		move->sprint = 1;
+		printf ("sprint");
+	}
+	return (0);
+}
+
+int					ft_expose(t_mlx *mlx)
+{
+	int		i;
+	int		n;
+
+	i = 0;
+	n = 16777215;
+	mlx->img = mlx_new_image(mlx->ptr, 640, 480);
+	mlx->data_img = mlx_get_data_addr(mlx->img, &mlx->bpixel, &mlx->size_line, &mlx->endian);
+	mlx->bpixel = mlx->bpixel / 8;
+	while (i < ((mlx->size_line / mlx->bpixel) * 480))
+	{
+		*(int *)&mlx->data_img[i * 4] = n;
+		//n += 1;
+		i++;
+	}
+	mlx_put_image_to_window(mlx->ptr, mlx->win, mlx->img, 0 ,0);
+	return (0);	
+}
+
+void				ft_init_struct_move(t_move *move)
+{
+	move->esc = 0;
+	move->foward = 0;
+	move->retreat = 0;
+	move->left = 0;
+	move->right = 0;
+	move->turn_left = 0;
+	move->turn_right = 0;
+	move->sprint = 0;
 }
 
 int					main(void)
 {
-	t_mlx	mlx;
-	int		i;
+	t_mlx		mlx;
+	t_move		move;	
 	
-	mlx.x = 320;
-	mlx.y = 420;
-	i = 0;
+	ft_init_struct_move(&move);
 	if ((mlx.ptr = mlx_init()) == NULL)
 		return (-1);
 	if ((mlx.win = mlx_new_window(mlx.ptr, 640, 480, "Cub2D")) == NULL)
 		return (-1);
-	mlx_hook(mlx.win, 2, 1L << 0, ft_keypress, &mlx);
-	mlx_hook(mlx.win, 3, 1L << 1, ft_keyrelease, &mlx);
+	mlx_hook(mlx.win, 2, 1L << 0, ft_keypress, &move);
+	mlx_hook(mlx.win, 3, 1L << 1, ft_keyrelease, &move);
+	mlx_hook(mlx.win, 12, 1L << 15, ft_expose, &mlx);
 	//mlx_hook(mlx.win, 17, 1L << 17, ft_destroy_window, &mlx);
 	mlx_loop(mlx.ptr);
 	return (0);
 }
 
 //https://code.woboq.org/qt5/include/X11/X.h.html
-
-/*	key_press (2, 0)
-	key_release (3 , 0)
-	mlx_hook
+/*
 	mlx.img = mlx_new_image(mlx.ptr, 640, 480);		
 	mlx.img = mlx_new_image(mlx.ptr, 640, 480);		
 	mlx.data_img = mlx_get_data_addr(mlx.img, &mlx.bpixel, &mlx.size_line, &mlx.endian);
